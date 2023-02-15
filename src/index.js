1c339d186f51535777e41b00ae92af39a773610f
Yasamin-Nourijelyani <yasamin.nourijelyani@mail.utoronto.ca>
2023-02-15 12:53:46 -0500

    firebase setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {initializeApp} from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyAeyuRFehSKfK-fY_Cd_auzCHIq6uVcVEo",
+    authDomain: "disoveryapp-6e148.firebaseapp.com",
+    projectId: "disoveryapp-6e148",
+    storageBucket: "disoveryapp-6e148.appspot.com",
+    messagingSenderId: "408140914826",
+    appId: "1:408140914826:web:1f2152aa8be73cdb00cd73"
+});
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+
